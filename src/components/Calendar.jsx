d72bd6c4980154943b77c0e67d8ca180cdeefe65
nishanttt1 <nishantkhadka394@gmail.com>
2026-09-02 18:22:58 +0545

    updated calendar page

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import notification from '../assets/notification.png';
+import notification from "../assets/notification.png";
 import "./Calendar.css";
 
 const Calendar = () => {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dates = Array.from({ length: 30 }, (_, i) => i + 1);
+  const upcomingEvents = [];
+
   return (
     <div className="calendar">
       <header className="header">
         <div className="title-left">
-          <span>DEAD & MEETING</span>
+          <span>DATE & MEETING</span>
           <h2>Calendar</h2>
         </div>
 
@@ -19,6 +23,49 @@ const Calendar = () => {
           <button className="profile-logo">RR</button>
         </div>
       </header>
+      <main className="calendar-content">
+        <section className="calendar-card">
+          <div className="weekdays">
+            {days.map((day) => (
+              <div className="weekday" key={day}>
+                {day}
+              </div>
+            ))}
+          </div>
+
+          <div className="dates-grid">
+            {dates.map((date) => (
+              <div
+                key={date}
+                className={`date-box ${date === 11 ? "active-date" : ""}`}
+              >
+                {date}
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <section className="upcoming-events">
+          <h2>Upcoming Events</h2>
+          <div className="events">
+            {upcomingEvents.length === 0 ? (
+              <div className="no-events">No events available</div>
+            ) : (
+              upcomingEvents.map((event, index) => (
+                <div className="event-card" key={index}>
+                  <div className={`event-date ${event.color}`}>
+                    {event.date}
+                  </div>
+                  <div className="event-info">
+                    <h3>{event.title} </h3>
+                    <p>{event.type} </p>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
